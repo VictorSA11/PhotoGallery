57829566369f81c1c3d8377f7782d5c80a0e1b3c
Victor Segura <viko_1995@2hotmail.com>
2020-06-18 20:38:37 -0600

    lab

--- app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
@@ -37,20 +37,20 @@ public class PhotoGalleryFragment extends Fragment {
 
     private class PhotoHolder extends  RecyclerView.ViewHolder {
          private TextView mTitleTextView;
-        private ImageView mItemImageView;
+      //  private ImageView mItemImageView;
 
         public PhotoHolder(View itemView) {
             super(itemView);
             //  mItemImageView = itemView.findViewById(R.id.gallery_item_imageView);
             mTitleTextView = (TextView) itemView;
         }
-        public void bindGalleryItem(GalleryItem item) {
+       public void bindGalleryItem(GalleryItem item) {
             mTitleTextView.setText(item.toString());
         }
 
-        public void bindDrawable(Drawable drawable) {
-            mItemImageView.setImageDrawable(drawable);
-        }
+       // public void bindDrawable(Drawable drawable) {
+          //  mItemImageView.setImageDrawable(drawable);
+        //}
     }
 
     private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
@@ -65,9 +65,9 @@ public class PhotoGalleryFragment extends Fragment {
         public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
            TextView textView = new TextView(getActivity());
            return new PhotoHolder(textView);
-//             LayoutInflater inflater = LayoutInflater.from(getActivity());
-//            View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
-//            return new PhotoHolder(view);
+          //   LayoutInflater inflater = LayoutInflater.from(getActivity());
+          // View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
+           // return new PhotoHolder(view);
 
         }
 
@@ -75,8 +75,8 @@ public class PhotoGalleryFragment extends Fragment {
         public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int position) {
            GalleryItem galleryItem = mGalleryItems.get(position);
              photoHolder.bindGalleryItem(galleryItem);
-//            Drawable placeHolder = getResources().getDrawable(R.drawable.bill_up_close);
-//            photoHolder.bindDrawable(placeHolder);
+          // Drawable placeHolder = getResources().getDrawable(R.drawable.bill_up_close);
+           //photoHolder.bindDrawable(placeHolder);
 
         }
 
